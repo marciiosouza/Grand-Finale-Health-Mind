72be1cacebf6d56d6da9a06e818a810f12363e38
Wagner  Morais <waguinhoxd4@gmail.com>
2023-11-07 20:56:56 -0300

    api

---
+++ HealthMindApi/HealthMindApi/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using apiHealthMind.Context;
+using apiHealthMind.Model;
+using apiHealthMind.Repository;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace apiHealthMind.Controllers
+{
+    
+    
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioRepository usuarioRepository;
+
+        public UsuarioController(AppDbContext dbx )    {
+            usuarioRepository = new UsuarioRepository(dbx);
+        }
+
+        [HttpPost("cadastrar")]
+        public ActionResult<UsuarioModel> Post([FromBody] UsuarioModel usuario)
+        {
+            try
+            {
+                usuarioRepository.InserirUsuario(usuario);
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Não foi possível cadastrar suspeito" });
+            }
+        }
+
+
+        
+    }
+}
