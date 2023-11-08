cd1deb6b77e3e42d3dc5b16b61f9294a351beae9
marciiosouza <marciiosouza@outlook.com>
2023-11-08 07:26:16 -0300

    Finalizando Refatoração da tela de cadastro

--- app/src/main/java/br/com/fiap/healfmind/screens/CadastroScreen.kt
+++ app/src/main/java/br/com/fiap/healfmind/screens/CadastroScreen.kt
@@ -3,29 +3,39 @@ package br.com.fiap.healfmind.screens
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -37,189 +47,212 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import br.com.fiap.healfmind.R
 import br.com.fiap.healfmind.database.repository.UsuarioRepository
 import br.com.fiap.healfmind.model.Usuarios
 import br.com.fiap.healfmind.service.RetrofitFactory
+import br.com.fiap.healfmind.ui.theme.blue_gradient
+import br.com.fiap.healfmind.ui.theme.purple_gradient
 import br.com.fiap.healfmind.viewModel.CadastroScreenViewModel
 import com.example.healf_mind.components.CaixaDeEntrada
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CadastroScreen(navController: NavController, cadastroScreenViewModel: CadastroScreenViewModel) {
-    // var de estado
+
     val nome by cadastroScreenViewModel.nome.observeAsState(initial = "")
     val email by cadastroScreenViewModel.email.observeAsState(initial = "")
     val senha by cadastroScreenViewModel.senha.observeAsState(initial = "")
-//    // Obter contexto atual
     val context = LocalContext.current
     val usuarioRepository = UsuarioRepository(context)
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Image(
-            painterResource(id = R.drawable.tela_login),
-            contentDescription = null,
-            contentScale = ContentScale.FillBounds,
-            modifier = Modifier.matchParentSize()
+    Box(modifier = Modifier
+        .background(
+            Brush.linearGradient(
+                colors = listOf(
+                    purple_gradient,
+                    blue_gradient
+
+                )
+            )
         )
+    )
 
-
-
+    {
         Column(
-            Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 40.dp),
             verticalArrangement = Arrangement.Center,
-
-
-            ) {
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
             Text(
-                text = "Para um cuidado \ncompleto com a sua saúde",
+                text = "Realizar Cadastro",
                 style = TextStyle(
                     fontSize = 22.sp,
-                    lineHeight = 21.sp,
                     fontFamily = FontFamily(Font(R.font.inter_bold)),
                     fontWeight = FontWeight(700),
                     color = Color(0xFFFFFFFF),
-                    textAlign = TextAlign.Center,
-
-                    )
+                )
             )
-            Spacer(modifier = Modifier.height(50.dp))
-            Card(
-                Modifier
-                    .width(340.dp)
-                    .height(451.dp)
-                    .background(
-                        color = Color(0xFFFAFAFA),
-                        shape = RoundedCornerShape(size = 70.dp)
-                    ),
-                elevation = CardDefaults.cardElevation(10.dp),
 
+            Spacer(modifier = Modifier.height(38.dp))
 
-                ) {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .padding(top = 20.dp, bottom = 20.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
+            OutlinedTextField(modifier = Modifier
+                .width(303.dp)
+                .height(50.dp)
+                .background(
+                    color = Color(0xFFFAFAFA),
+                    shape = RoundedCornerShape(size = 10.dp)
+                )
+                .border(0.dp, Color.Transparent),
+
+                value = "",
+                onValueChange = { },
+
+                placeholder = {
                     Text(
-                        text = "Cadastrar",
+                        text = "Nome",
                         style = TextStyle(
-                            fontSize = 22.sp,
-                            fontFamily = FontFamily(Font(R.font.inter_bold)),
-                            fontWeight = FontWeight(700),
-                            color = Color(0xFF005DF9),
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            fontWeight = FontWeight(400),
+                            color = Color(0x4A000000),
                         )
                     )
-                    CaixaDeEntrada(
-                        label = "Nome",
-                        placeholder = "Insira seu nome completo",
-                        value = nome,
-                        keyboardType = KeyboardType.Text,
-                        modifier = Modifier
-                            .shadow(
-                                elevation = 4.dp,
-                                spotColor = Color(0x1A000000),
-                                ambientColor = Color(0x1A000000)
-                            ),
-                        atualizarValor = {
-                            cadastroScreenViewModel.onNameChanged(it)
-                        },
-                        error = false
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Transparent,
+                )
+            )
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            OutlinedTextField(modifier = Modifier
+                .width(303.dp)
+                .height(50.dp)
+                .background(
+                    color = Color(0xFFFAFAFA),
+                    shape = RoundedCornerShape(size = 10.dp)
+                )
+                .border(0.dp, Color.Transparent),
+
+                value = "",
+                onValueChange = { },
+
+                placeholder = {
+                    Text(
+                        text = "E-mail",
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            fontWeight = FontWeight(400),
+                            color = Color(0x4A000000),
+                        )
                     )
-                    CaixaDeEntrada(
-                        label = "Email",
-                        placeholder = "Digite seu Email",
-                        value = email,
-                        keyboardType = KeyboardType.Email,
-                        modifier = Modifier
-                            .shadow(
-                                elevation = 4.dp,
-                                spotColor = Color(0x1A000000),
-                                ambientColor = Color(0x1A000000)
-                            ),
-                        atualizarValor = {
-                            cadastroScreenViewModel.onEmailChanged(it)
-                        },
-                        error = false
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Transparent,
+                )
+            )
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            OutlinedTextField(modifier = Modifier
+                .width(303.dp)
+                .height(50.dp)
+                .background(
+                    color = Color(0xFFFAFAFA),
+                    shape = RoundedCornerShape(size = 10.dp)
+                )
+                .border(0.dp, Color.Transparent),
+
+                value = "",
+                onValueChange = { },
+
+                placeholder = {
+                    Text(
+                        text = "Senha",
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            fontWeight = FontWeight(400),
+                            color = Color(0x4A000000),
+                        )
                     )
-                    CaixaDeEntrada(
-                        label = "Senha",
-                        placeholder = "Digite uma senha ",
-                        value = senha,
-                        keyboardType = KeyboardType.Password,
-                        modifier = Modifier
-                            .shadow(
-                                elevation = 4.dp,
-                                spotColor = Color(0x1A000000),
-                                ambientColor = Color(0x1A000000)
-                            ),
-                        atualizarValor = {
-                            cadastroScreenViewModel.onSenhaChanged(it)
-                        },
-                        error = false
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.Transparent,
+                )
+            )
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .width(303.dp)
+                    .height(50.dp),
+                shape = RoundedCornerShape(size = 10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.azul)
+                )
+
+            ) {
+                Text(text = "Cadastrar")
+            }
+
+            Spacer(modifier = Modifier.height(17.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .width(303.dp)
+                    .height(50.dp),
+                shape = RoundedCornerShape(size = 10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.white)
+                )
+            ) {
+                Text(
+                    text = "Entrar com o Google",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.inter_regular)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF000000)
                     )
-                    Button(
-                        onClick = {
-                            var call = RetrofitFactory().getUsuarioService().inserirUsuario(
-                                nome = nome,
-                                email = email,
-                                senha = senha
-                            )
+                )
 
-                            call.enqueue(object : Callback<Usuarios>{
-                                override fun onResponse(
-                                    call: Call<Usuarios>,
-                                    response: Response<Usuarios>
-                                ) {
-                                    Log.i("C##1", "onResponse: ${response.body()}")
-                                }
+                Spacer(modifier = Modifier.width(107.dp))
 
-                                override fun onFailure(call: Call<Usuarios>, t: Throwable) {
-                                    Log.i("C##111", "onResponse: ${t.message}")
-                                }
-
-                            })
-                            //val usuario = Usuarios(id = 0, nome = nome, email = email)
-                            //usuarioRepository.salvar(usuario)
-                        },
-                        Modifier
-                            .shadow(
-                                elevation = 4.dp,
-                                spotColor = Color(0x1A000000),
-                                ambientColor = Color(0x1A000000)
-                            )
-                            .width(231.dp)
-                            .height(50.dp)
-                            .background(
-                                color = Color(0xFF005DF9),
-                                shape = RoundedCornerShape(size = 25.dp)
-                            )
-
-                    ) {
-                        Text(text = "Cadastrar")
-                    }
-                }
-
+                Image(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = "Seta para esquerda",
+                    modifier = Modifier.size(15.dp)
+                )
             }
         }
     }
 }
 
 
-//@Preview(showSystemUi =  true , showBackground = true)
-//@Composable
-//fun CadastroScreenPreview(){
-//    CadastroScreen()
-//}
+@Composable
+fun Greeting(name: String) {
+}
+
+@Preview
+@Composable
+
+
+fun LoginScreenPreview() {
+    val cadastroScreenViewModel = CadastroScreenViewModel()
+    CadastroScreen(cadastroScreenViewModel = cadastroScreenViewModel, navController = rememberNavController())
+}
